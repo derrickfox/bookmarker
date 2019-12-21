4e9796a7b3a567832a67183fd836980bf0870328
Me <fox.derrick@gmail.com>
2019-12-21 16:21:11 -0500

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ModuleRegistry, GridApi} from '@ag-grid-community/core';
+import *  as  data from './usStates.json';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'peoplePower';
+export class AppComponent implements OnInit {
+	title = 'peoplePower';
+	jsonData = data['default'];
+	columnDefs = [
+		{ headerName: 'State', field: 'state' },
+		{ headerName: 'Population', field: 'population' }
+	];
+	rowData = [];
+
+	ngOnInit() {
+		console.log(this.jsonData);
+		for (let row of this.jsonData) {
+			const state = row.NAME;
+			const population = row.POPESTIMATE2018;
+			let stateObject = {
+				'state': state,
+				'population': population
+			};
+			this.rowData.push(stateObject);
+		}
+	}
+
+	onGridReady(params: GridApi) {
+		// params.api.sizeColumnsToFit();
+	 }
+
+	onStateClick(element: HTMLElement) {
+		console.log('element', element);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms"; 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    HttpClientModule,
+    AgGridModule.withComponents([])
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
