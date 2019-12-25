f6b5da6611c407324d28c7e2a70092b9a42161d3
Me <fox.derrick@gmail.com>
2019-12-25 17:18:58 -0500

    merry xmas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ModuleRegistry, GridApi} from '@ag-grid-community/core';
-import { AllCommunityModules, Module } from "@ag-grid-community/all-modules";
 import "@ag-grid-community/all-modules/dist/styles/ag-grid.css";
 import "@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css";
-import { HttpClient } from "@angular/common/http";
-import *  as  data from './usStates.json';
 
 @Component({
 	selector: 'app-root',
